5c9ced66a0a6271c8ca0ece8e20425edda042022
nheri ines <inesneri2013@gmail.com>
2025-03-29 17:21:38 +0100

    Modifiche

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa'],  // Trattare react-icons/fa come esterno
+    },
+  },
 });
